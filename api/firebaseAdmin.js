e6a45a7d47d852e03bf2fa335595efdf1e18ad5c
Maaz16621 <maazhanif1997@gmail.com>
2025-12-02 12:59:05 +0500

    updated api

--- api/firebaseAdmin.js
+++ api/firebaseAdmin.js
@@ -1,26 +1,12 @@
 const admin = require('firebase-admin');
+const { getFirestore } = require('firebase-admin/firestore');
 
 // Initialize default app
 if (admin.apps.length === 0) {
   admin.initializeApp();
 }
 
-// Initialize Doha DB app
-let dohaDb;
-if (admin.apps.some(app => app.name === 'doha-db')) {
-  dohaDb = admin.app('doha-db').firestore();
-} else {
-  // When deployed to a Google Cloud environment like App Hosting,
-  // the service account is discovered automatically.
-  // For local development, you might need to set GOOGLE_APPLICATION_CREDENTIALS
-  // to the path of your service account key file.
-  const dohaApp = admin.initializeApp({
-    databaseId: 'doha-db'
-  }, 'doha-db');
-  dohaDb = dohaApp.firestore();
-}
-
-
-const db = admin.firestore();
+const db = getFirestore(); // default db
+const dohaDb = getFirestore('doha-db');
 
 module.exports = { admin, db, dohaDb };
