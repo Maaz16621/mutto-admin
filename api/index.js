81208e03c7d0f257663054f0dba1227fc4b89cbd
Maaz16621 <maazhanif1997@gmail.com>
2025-12-28 21:06:18 +0500

    api updated

--- api/index.js
+++ api/index.js
@@ -7,7 +7,7 @@ const placeDetailsRoute = require('./placeDetails');
 const nominatimProxyRoute = require('./nominatimProxy');
 const googleToOsmRoute = require('./googleToOsm');
 const getServicesRoute = require('./getServices');
-const bookingCompleted = require('./bookingCompletedmpleted');
+const bookingCompleted = require('./bookingCompleted');
 const { runUpdate } = require('./updateVehicleType');
 
 
